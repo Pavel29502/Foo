ea7473e5a812a833b00d4ce4b0f06b78057881d6
pavel khat <khatpavel@gmail.com>
2025-04-11 21:49:29 +0300

    ..

--- src/main/java/com/example/Foo/Foo.java
+++ src/main/java/com/example/Foo/Foo.java
@@ -3,22 +3,34 @@ import java.util.concurrent.CountDownLatch;
 
 public class Foo {
 
-    CountDownLatch first = new CountDownLatch(1);
-    CountDownLatch second = new CountDownLatch(1);
+    CountDownLatch firstCount = new CountDownLatch(1);
+    CountDownLatch secondCount = new CountDownLatch(1);
 
     public void first(Runnable r) {
         r.run();
-        first.countDown();
+        firstCount.countDown();
         System.out.println("first");
     }
 
     public void second(Runnable r) {
+        try {
+            firstCount.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         r.run();
-        second.countDown();
+        secondCount.countDown();
         System.out.println("second");
     }
 
     public void third(Runnable r) {
+        try {
+            secondCount.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        r.run();
+        firstCount.countDown();
         System.out.println("third");
     }
 }
